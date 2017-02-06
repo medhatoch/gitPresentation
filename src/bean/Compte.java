dcf14d98958aabaadf0042d028774d4d69cfa3f7
medhatoch <medamine.hatoch@gmail.com>
2017-02-06 17:11:25 +0000

    adding getter/setter

--- src/bean/Compte.java
+++ src/bean/Compte.java
@@ -10,5 +10,36 @@ package bean;
  * @author ESDAIRI
  */
 public class Compte {
+    private String id;
+    private Double solde;
+
+    public Compte(String id, Double solde) {
+        this.id = id;
+        this.solde = solde;
+    }
+
+    public Compte() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(Double solde) {
+        this.solde = solde;
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" + "id=" + id + ", solde=" + solde + '}';
+    }
     
 }
